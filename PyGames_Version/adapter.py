791a62328a8a2f6f01ff4c324d112ebf4d92af7e
OOgaboogaboog <74546565+OOgaboogaboog@users.noreply.github.com>
2021-02-28 22:28:58 -0500

    trying a thing

--- PyGames_Version/adapter.py
+++ PyGames_Version/adapter.py
@@ -13,7 +13,8 @@ def Adapters():
         adapters = ("wlan0","eth0","eth1","eth2","ed","esh1","esh2") 
         
     else:    
-        adapters_NoParse = subprocess.Popen(['./adapter_check.sh'],shell=True, stdout=subprocess.PIPE, universal_newlines=True).communicate()[0]
+        adapters_NoParse = subprocess.Popen(['./adapter_check.sh'],shell=True, stdout=subprocess.PIPE, universal_newlines=True)
+        adapters_NoParse = adapters_NoParse.communicate()[0]
         adapters = adapters_NoParse.strip("\n")
         adapters = adapters.split(" ")
 
